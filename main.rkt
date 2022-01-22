6cb2769314db5d78f352299b5b1105910b8763f7
Trirst <abeces968@gmail.com>
2022-01-23 00:47:13 +0700

    Minor fix for main.rkt

--- main.rkt
+++ main.rkt
@@ -6,12 +6,16 @@
          (only-in 2htdp/image save-image)
          "typed-convolve.rkt")
 
-;; (define flmp (bitmap->flomap (read-bitmap "img.jpg")))
+;; (define flmp (bitmap->flomap (read-bitmap "scaled.jpg")))
 
 (define (render-convoluted flmp kernel)
   (flomap->bitmap (typed-flomap-convolve flmp
                                          kernel)))
 
-;; (define t (transform flmp (flvector 0. -1. 0. -1. 5. -1. 0. -1. 0.)))
+;; (define t (render-convoluted flmp (flvector 0. 0. 0. 0. 0.
+;;                                             0. 0. 0. 0. 0.
+;;                                             0. 0. 1. 0. 0.
+;;                                             0. 0. 0. 0. 0.
+;;                                             0. 0. 0. 0. 0.)))
 
 ;; (save-image t "t.png")
