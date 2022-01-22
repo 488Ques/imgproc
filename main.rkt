2b2bb5115a44ff6d28a7ebb4251605d1468ec2af
Trirst <abeces968@gmail.com>
2022-01-22 23:29:24 +0700

    Example usage

--- main.rkt
+++ main.rkt
@@ -1,39 +1,17 @@
 #lang racket
-(require 2htdp/image
-         "matrix.rkt"
-         "processing.rkt"
-         "kernels.rkt")
 
-(define target (bitmap/file "img.jpg"))
+(require (only-in racket/draw read-bitmap)
+         (only-in images/flomap bitmap->flomap flomap->bitmap)
+         (only-in racket/flonum flvector)
+         (only-in 2htdp/image save-image)
+         "typed-convolve.rkt")
 
-(define (transform image kernel)
-  (define COL (image-width image))
-  (define ROW (image-height image))
-  (define pixels (image->color-list image))
-  
-  (define (extract-channels clrlst)
-    (define (extract-channel channel)
-      (list->matrix (map (lambda (clr) (channel clr)) clrlst) COL))
-    (values (extract-channel color-red)
-            (extract-channel color-green)
-            (extract-channel color-blue)))
-  
-  (define (process-channels R G B kernel)
-    (define (process-channel channel)
-      (processing channel kernel))
-    (values (process-channel R)
-            (process-channel G)
-            (process-channel B)))
+;; (define flmp (bitmap->flomap (read-bitmap "img.jpg")))
 
-  (define (join-channels R G B)
-    (for/list ([r R]
-               [g G]
-               [b B])
-      (make-color r g b)))
-  
-  (define-values (R G B) (extract-channels pixels))
-  (define-values (proc-R proc-G proc-B) (process-channels R G B kernel))
-  (define proc-clrlst (join-channels proc-R
-                                     proc-G
-                                     proc-B))
-  (color-list->bitmap proc-clrlst COL ROW))
+(define (render-convoluted flmp kernel)
+  (flomap->bitmap (typed-flomap-convolve flmp
+                                         kernel)))
+
+;; (define t (transform flmp (flvector 0. -1. 0. -1. 5. -1. 0. -1. 0.)))
+
+;; (save-image t "t.png")
